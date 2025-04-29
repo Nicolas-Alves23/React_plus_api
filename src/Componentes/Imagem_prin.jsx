c24abe5ab320f92716237cebfee14f9f00af0c2f
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-04-29 20:51:22 -0300

    Make modifications

--- src/Componentes/Imagem_prin.jsx
+++
@@ -1,6 +0,0 @@
-import { style } from './Imagem_prin.module.css'
-
-export function Imagem_prin(){
-
-}
-
--- src/Componentes/Loading.jsx
+++ src/Componentes/Loading.jsx
@@ -3,22 +3,14 @@ import loading from '../img/loading.svg';
 import styles from './Loading.module.css';
 
 export function Loading() {
-    const [text, setText] = useState('');
     const [showImg, setShowImg] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setText('Espere 3 segundinhos por favor');
-            setShowImg(false);
-        }, 30000);
-    }, []);
 
     return (
         <div className={styles.loadingContainer}>
             {showImg ? (
                 <img src={loading} alt="Carregando..." />
             ) : (
-                <h3>{text}</h3>
+                <h3>Espere um pouco</h3>
             )}
         </div>
     );
