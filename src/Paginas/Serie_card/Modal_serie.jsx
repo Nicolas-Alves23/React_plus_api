92370fdf6bf677bdbb812ac4f1b649fc49d57bc8
Nicolas-Alves23 <nicolasvilelabarros@gmail.com>
2025-04-30 10:05:34 -0300

    ...

--- src/Paginas/Serie_card/Modal_serie.jsx
+++ src/Paginas/Serie_card/Modal_serie.jsx
@@ -1,4 +1,4 @@
-import estilo from './Modal_serie.module.css';
+import style from './Modal_serie.module.css';
 
 export function Modal_serie({ serie, onClose }) {
     if (!serie) {
@@ -6,39 +6,23 @@ export function Modal_serie({ serie, onClose }) {
     }
 
     return (
-        <div className={estilo.modalback}>
-            <div className={estilo.modalConteiner}>
-                <div className={estilo.modalheader}>
+        <div className={style.modalback}>
+            <div className={style.modalConteiner}>
+                <div className={style.modalheader}>
                     <button onClick={onClose}>X</button>
-
-                    {serie.trailerKey ? (
-                        <div>
-                            <iframe
-                                width="100%"
-                                height="700px"
-                                src={`https://www.youtube.com/embed/${serie.trailerKey}?autoplay=1`}
-                                title="Trailer"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                            ></iframe>
-                        </div>
-                    ) : (
-                        <p>Trailer não disponível.</p>
-                    )}
                 </div>
-                <div className={estilo.numbers_about_film}>
-                    <div className={estilo.text_in_modal}>
+                <div className={style.numbers_about_film}>
+                    <div className={style.text_in_modal}>
                         <h2>Opinião do Pública</h2>
-                        <p className={estilo.numbers}>{serie.vote_average}</p>
+                        <p className={style.numbers}>{serie.vote_average}</p>
                     </div>
-                    <div className={estilo.text_in_modal}>
+                    <div className={style.text_in_modal}>
                         <h2>Quantidade de Votos</h2>
-                        <p className={estilo.numbers}>{serie.vote_count}</p>
+                        <p className={style.numbers}>{serie.vote_count}</p>
                     </div>
-                    <div className={estilo.text_in_modal}>
+                    <div className={style.text_in_modal}>
                         <h2>Popularidade</h2>
-                        <p className={estilo.numbers}>{serie.popularity}</p>
+                        <p className={style.numbers}>{serie.popularity}</p>
                     </div>
                 </div>
             </div>
